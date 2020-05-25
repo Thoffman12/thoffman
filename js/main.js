2ae6732f549a529d04ecd3fc61f6520ee931c30b
Hoffman <niac1233@gmail.com>
2020-05-25 16:26:47 +0300

    Наш первый коммит

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+console.log(leftBorderWidth);
+
+let persone = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone.name);
+
+let arr = ['plum.png','orange.jpeg','apple.bmp'];
+
+console.log(arr[0]);
